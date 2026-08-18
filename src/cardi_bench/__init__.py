956561ef79db819620aa507d97fe8c5959d4b830
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-18 12:25:53 +0300

    Expose benchmark result contract

--- src/cardi_bench/__init__.py
+++ src/cardi_bench/__init__.py
@@ -1,6 +1,7 @@
 """CardiBench: reproducible cardiac benchmark infrastructure."""
 
 from .adapters import AdapterResult, adapt_geo_samples
+from .benchmark import SplitPlan, assert_benchmark_safe, label_counts, plan_group_split
 from .catalog import BENCHMARK_FAMILIES, BenchmarkFamily, get_benchmark_family
 from .manifest import assert_valid_manifest, validate_manifest
 from .materialize import MaterializedBenchmark, materialize
@@ -9,8 +10,8 @@ from .policies import POLICIES, Policy, available_policies, plan_policy_split
 from .provenance import ProvenanceRecord, canonical_metadata_hash, make_provenance
 from .quality import QualityReport, assess_dataset
 from .readiness import ReadinessReport, assess_readiness
-from .release import AuditResult, audit_repository_paths
 from .registry import Sample, summarize_samples, validate_no_group_leakage
+from .results import BenchmarkResult
 from .samples import SampleRecord, attach_normalized_label, normalize_condition
 from .splits import make_group_split, split_summary
 from .splitting import group_key, make_grouped_split, split_statistics
@@ -21,11 +22,11 @@ __all__ = [
     "validate_no_group_leakage", "summarize_samples", "group_key",
     "make_grouped_split", "split_summary", "split_statistics",
     "QualityReport", "assess_dataset", "ReadinessReport", "assess_readiness",
-    "AuditResult", "audit_repository_paths",
     "LABELS", "CELL_CONTEXTS", "REGIONS", "MODALITIES",
     "validate_manifest", "assert_valid_manifest",
     "BenchmarkFamily", "BENCHMARK_FAMILIES", "get_benchmark_family",
     "ProvenanceRecord", "canonical_metadata_hash", "make_provenance",
     "Policy", "POLICIES", "available_policies", "plan_policy_split",
-    "MaterializedBenchmark", "materialize",
+    "SplitPlan", "plan_group_split", "assert_benchmark_safe", "label_counts",
+    "MaterializedBenchmark", "materialize", "BenchmarkResult",
 ]
