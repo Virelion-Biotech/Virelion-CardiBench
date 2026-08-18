a2204ae52f834a49d0d0a3e0c39c3b132355842f
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-18 10:08:01 +0300

    Expose catalog and provenance APIs

--- src/cardi_bench/__init__.py
+++ src/cardi_bench/__init__.py
@@ -1,8 +1,10 @@
 """CardiBench: reproducible cardiac benchmark infrastructure."""
 
 from .adapters import AdapterResult, adapt_geo_samples
+from .catalog import BENCHMARK_FAMILIES, BenchmarkFamily, get_benchmark_family
 from .manifest import assert_valid_manifest, validate_manifest
 from .ontology import CELL_CONTEXTS, LABELS, MODALITIES, REGIONS, canonical_label
+from .provenance import ProvenanceRecord, canonical_metadata_hash, make_provenance
 from .quality import QualityReport, assess_dataset
 from .registry import Sample, summarize_samples, validate_no_group_leakage
 from .samples import SampleRecord, attach_normalized_label, normalize_condition
@@ -13,8 +15,9 @@ __all__ = [
     "Sample", "SampleRecord", "AdapterResult", "adapt_geo_samples",
     "normalize_condition", "attach_normalized_label", "canonical_label",
     "validate_no_group_leakage", "summarize_samples", "group_key",
-    "make_grouped_split", "split_summary", "make_grouped_split",
-    "split_statistics", "QualityReport", "assess_dataset",
-    "LABELS", "CELL_CONTEXTS", "REGIONS", "MODALITIES",
+    "make_grouped_split", "split_summary", "split_statistics",
+    "QualityReport", "assess_dataset", "LABELS", "CELL_CONTEXTS", "REGIONS", "MODALITIES",
     "validate_manifest", "assert_valid_manifest",
+    "BenchmarkFamily", "BENCHMARK_FAMILIES", "get_benchmark_family",
+    "ProvenanceRecord", "canonical_metadata_hash", "make_provenance",
 ]
