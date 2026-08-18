d49ea504fd505aaba2da285c3e704d19adffb35a
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-18 12:17:35 +0300

    Expose readiness assessment

--- src/cardi_bench/__init__.py
+++ src/cardi_bench/__init__.py
@@ -8,6 +8,7 @@ from .ontology import CELL_CONTEXTS, LABELS, MODALITIES, REGIONS, canonical_labe
 from .policies import POLICIES, Policy, available_policies, plan_policy_split
 from .provenance import ProvenanceRecord, canonical_metadata_hash, make_provenance
 from .quality import QualityReport, assess_dataset
+from .readiness import ReadinessReport, assess_readiness
 from .registry import Sample, summarize_samples, validate_no_group_leakage
 from .samples import SampleRecord, attach_normalized_label, normalize_condition
 from .splits import make_group_split, split_summary
@@ -18,7 +19,8 @@ __all__ = [
     "normalize_condition", "attach_normalized_label", "canonical_label",
     "validate_no_group_leakage", "summarize_samples", "group_key",
     "make_grouped_split", "split_summary", "split_statistics",
-    "QualityReport", "assess_dataset", "LABELS", "CELL_CONTEXTS", "REGIONS", "MODALITIES",
+    "QualityReport", "assess_dataset", "ReadinessReport", "assess_readiness",
+    "LABELS", "CELL_CONTEXTS", "REGIONS", "MODALITIES",
     "validate_manifest", "assert_valid_manifest",
     "BenchmarkFamily", "BENCHMARK_FAMILIES", "get_benchmark_family",
     "ProvenanceRecord", "canonical_metadata_hash", "make_provenance",
